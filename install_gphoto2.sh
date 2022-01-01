d8070864380d24ec602b673a6c8fe56f6fed1e18
josmet52 <joseph.metrailler@bluewin.ch>
2022-01-01 15:26:52 +0100

    updated

---
+++ install_gphoto2.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install gphoto2 -y
+sudo chmod -x /usr/lib/gvfs/gvfs-gphoto2-volume-monitor
+sudo apt install libgphoto2-dev -y
+sudo apt install swig -y
+sudo apt install mencoder -y
+
--- timelapse.sh
+++ timelapse.sh
@@ -1,8 +1,8 @@
 #! bash
-sudo gphoto2 --capture-image-and-download --frames 3000 --interval 12 --filename photos/%06n.jpg
+sudo gphoto2 --capture-image-and-download --frames 10 --interval 30 --filename photos/%06n.jpg
 cd photos
 sudo mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse.mp4 -mf type=jpeg:fps=24 mf://*.jpg
-sudo rm *.jpg
+# sudo rm *.jpg
 sudo mv timelapse.mp4 "../videos/tl-$(date +"%Y%m%d_%H%M%S").mp4"
 cd ..
 
